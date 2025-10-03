d238a22735303dd74fa7142332f89a949f228ce3
goldy0759 <goldybhatia759@gmail.com>
2025-10-03 18:12:00 +0530

    feat: Initialize backend structure with MongoDB connection, user and product models, and seeder script
    - Added package.json scripts for server and client development
    - Created MongoDB connection configuration
    - Implemented user and product data models
    - Developed order model to handle order-related data
    - Added user seeder with hashed passwords for initial data
    - Implemented data import and destroy functionality in seeder script
    - Created example environment file for configuration

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom";
-import products from "../products";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
