76523833fc462049c0d0e206fe7446983c5333be
goldy0759 <goldybhatia759@gmail.com>
2025-10-26 18:57:19 +0530

    feat: Implement order management functionality with order routes and controller, add payment and shipping screens, and integrate checkout steps

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PrivateRoute from "./components/PrivateRoute";
+import PaymentScreen from "./screens/PaymentScreen";
 
 import {
   createBrowserRouter,
@@ -30,7 +32,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+      </Route>
     </Route>
   )
 );
