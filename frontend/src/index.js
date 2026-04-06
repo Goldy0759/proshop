2b66ca5a42498ccfb022bcf39a013f0bcc20f1cf
Navjot Singh <goldybhatia759@gmail.com>
2026-04-06 16:14:01 +0530

    feat: Add OrderScreen component and integrate order details retrieval in orderApiSlice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PrivateRoute from "./components/PrivateRoute";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderSceen";
 
 import {
   createBrowserRouter,
@@ -37,6 +38,7 @@ const router = createBrowserRouter(
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
       </Route>
     </Route>
   )
